14fdf8da9435af1b461408c029c3b19faecfa245
Dennis Eisenfeld <inf3780@hs-worms.de>
2022-04-19 19:04:36 +0200

    Add Helpers.cs

---
+++ Assets/Modules/Helpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Modules
+{
+    public class Helpers
+    {
+        public static int Clamp(int value, int min, int max)
+        {
+            if (value.CompareTo(min) < 0) {
+                return min;
+            }
+
+            if (value.CompareTo(max) > 0) {
+                return max;
+            }
+            
+            return value;
+        }
+    }
+}
