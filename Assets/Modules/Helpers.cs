20dfc72d1bcacd619ff0ae5cff4dfca7b7fb762c
Dennis Eisenfeld <inf3780@hs-worms.de>
2022-05-15 18:47:33 +0200

    Feature/state system

---
+++ Assets/Modules/Helpers.cs
@@ -0,0 +1,28 @@
+﻿namespace Modules
+{
+    /// <summary>
+    /// A static class supplying various helper functions.
+    /// </summary>
+    public static class Helpers
+    {
+        /// <summary>
+        /// This method is used to clamp a given value, so it fits into the given range.
+        /// </summary>
+        /// <param name="value">The value that should be clamped.</param>
+        /// <param name="min">The lower boundary.</param>
+        /// <param name="max">The upper boundary.</param>
+        /// <returns></returns>
+        public static double Clamp(double value, double min, double max)
+        {
+            if (value.CompareTo(min) < 0) {
+                return min;
+            }
+
+            if (value.CompareTo(max) > 0) {
+                return max;
+            }
+            
+            return value;
+        }
+    }
+}
---
+++ Assets/Modules/State.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace Modules
+{
+    /// <summary>
+    /// Class <c>State</c> represents a value between <c>RangeMin</c> and <c>RangeMax</c>, which can be increased, decreased,
+    /// or be dependent on other state values.
+    /// </summary>
+    public class State : BaseState
+    {
+        /// <inheritdoc />
+        public override double RangeMin { get; }
+
+        /// <inheritdoc />
+        public override double RangeMax { get; }
+
+        /// <inheritdoc />
+        public override List<IStateDependency> Dependencies { get; }
+
+        /// <summary>
+        /// This method updates the state value by iterating through all the <c>Dependencies</c>,
+        /// adding all the weighted dependency's states together.
+        /// </summary>
+        public override void UpdateStateValue()
+        {
+            if (Dependencies.Count == 0)
+            {
+                return;
+            }
+
+            double newStateValue = 0;
+            
+            foreach (IStateDependency stateDependency in Dependencies)
+            {
+                double weightedDependencyValue = stateDependency.State.Value * stateDependency.Weighting;
+                newStateValue += weightedDependencyValue;
+            }
+
+            Value = newStateValue;
+        }
+
+        /// <summary>
+        /// This constructor initializes a new State with the given arguments.
+        /// </summary>
+        /// <param name="value">The starting value.</param>
+        /// <param name="rangeMin">The lowest possible state value.</param>
+        /// <param name="rangeMax">The highest possible state value.</param>
+        /// <param name="dependencies">A list of <c>IStateDependency</c> instances</param>
+        public State(double value, double rangeMin, double rangeMax, List<IStateDependency> dependencies = null)
+        {
+            RangeMin = rangeMin;
+            RangeMax = rangeMax;
+            Value = value;
+            Dependencies = dependencies;
+        }
+    }
+}
