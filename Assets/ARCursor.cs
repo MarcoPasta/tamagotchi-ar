a5ca84bab1ba344af97d59587010457351b0a356
Luca Chiarelli <inf3398@hs-worms.de>
2022-05-21 02:05:24 +0200

    move camera into script, create androido prefab and fix it on center

--- Assets/ARCursor.cs
+++ Assets/ARCursor.cs
@@ -9,16 +9,17 @@ public class ARCursor : MonoBehaviour
     public GameObject cursorChildObject;
     public GameObject objectToPlace;
     public ARRaycastManager raycastManager;
+    public Camera mainCamera;
 
     public bool useCursor = true; 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         cursorChildObject.SetActive(useCursor);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (useCursor)
         {
@@ -29,23 +30,23 @@ public class ARCursor : MonoBehaviour
         {
             if (useCursor)
             {
-                GameObject.Instantiate(objectToPlace, transform.position, transform.rotation);
+                Instantiate(objectToPlace, transform.position, transform.rotation);
             }
             else
             {
-                List<ARRaycastHit> hits = new List<ARRaycastHit>();
+                var hits = new List<ARRaycastHit>();
                 raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.Planes);
                 if (hits.Count > 0)
                 {
-                    GameObject.Instantiate(objectToPlace, hits[0].pose.position, hits[0].pose.rotation);
+                    Instantiate(objectToPlace, hits[0].pose.position, hits[0].pose.rotation);
                 }
             }
         }
     }
 
-    void UpdateCursor()
+    private void UpdateCursor()
     {
-        Vector2 screenPosition = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
+        Vector2 screenPosition = mainCamera.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
         raycastManager.Raycast(screenPosition, hits, TrackableType.Planes);
         if (hits.Count > 0)
