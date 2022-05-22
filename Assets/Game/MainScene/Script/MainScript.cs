4dd521e764995cd2a911e45ca8bbb4a675cf264f
Dennis Eisenfeld <inf3780@hs-worms.de>
2022-05-22 21:36:00 +0200

    Refactor namespace

--- Assets/Game/MainScene/Script/MainScript.cs
+++ Assets/Game/MainScene/Script/MainScript.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MainScript : MonoBehaviour
+namespace Game.MainScene.Script
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MainScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
         {
             var identifier = SystemInfo.deviceModel;
     
@@ -32,9 +32,10 @@ public class MainScript : MonoBehaviour
         }
 
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
