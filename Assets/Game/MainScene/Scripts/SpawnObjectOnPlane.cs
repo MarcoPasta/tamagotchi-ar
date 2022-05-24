75bb02380b48eaed0654cab0fa3fb412fcf3894d
Dennis Eisenfeld <inf3780@hs-worms.de>
2022-05-24 21:33:10 +0200

    Add debugging capabilities

--- Assets/Game/MainScene/Scripts/SpawnObjectOnPlane.cs
+++ Assets/Game/MainScene/Scripts/SpawnObjectOnPlane.cs
@@ -25,13 +25,17 @@ namespace Game.MainScene.Scripts
 
         private static bool TryGetTouchPosition(out Vector2 touchPosition)
         {
+            Debug.Log("Trying to get a touch position...");
             if (Input.touchCount > 0)
             {
                 touchPosition = Input.GetTouch(0).position;
+                Debug.Log($"Touch occured at position {touchPosition}.");
                 return true;
             }
-
+            
+            
             touchPosition = default;
+            Debug.Log($"No touch occured. touchPosition = {touchPosition}");
             return false;
         }
 
@@ -43,16 +47,21 @@ namespace Game.MainScene.Scripts
                 return;
             }
 
+            Debug.Log($"Casting ray on touch position: {touchPosition}");
+            
             if (_raycastManager.Raycast(touchPosition, _raycastHits, TrackableType.PlaneWithinPolygon))
             {
                 var hitPose = _raycastHits[0].pose;
+                Debug.Log($"Hit pose: {hitPose}.");
 
                 if (_spawnedObject == null)
                 {
+                    Debug.Log($"Trying to instantiate prefab at {hitPose.position} with rotation {hitPose.rotation}.");
                     _spawnedObject = Instantiate(placeablePrefab, hitPose.position, hitPose.rotation);
                 }
                 else
                 {
+                    Debug.Log($"Moving {_spawnedObject} to {hitPose.position} with rotation {hitPose.rotation}.");
                     _spawnedObject.transform.position = hitPose.position;
                     _spawnedObject.transform.rotation = hitPose.rotation;
                 }
