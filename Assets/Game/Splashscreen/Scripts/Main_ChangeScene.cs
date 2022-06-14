c2b323feb506f6822f69ad04d92ddbb763087042
Luca Chiarelli <inf3398@hs-worms.de>
2022-06-14 18:46:59 +0200

    create a good looking splashscreen :thumbsup: :100:

---
+++ Assets/Game/Splashscreen/Scripts/Main_ChangeScene.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_ChangeScene : MonoBehaviour
+{
+    
+    public bool changeTrigger = false;
+
+    public int changeToSceneID;
+
+    private bool startLoading = false;
+    
+    public GameObject refAnchor;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startLoading && changeTrigger )
+        {
+            startLoading = true;
+            SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+        }
+    }
+
+
+    // public void changeSceneTo(int id)
+    // {
+    //     SceneManager.LoadScene(id);
+    // }
+}
